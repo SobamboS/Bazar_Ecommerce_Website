a670854604528f6377bf49c53af6cf215908a050
SobamboS <sobamboolusola1152@gmail.com>
2023-03-17 00:27:26 +0100

    Product and Cart Services

--- src/main/java/com/appsdeveloperblog/app/ws/mobileappws/Cart/CartItem.java
+++
@@ -1,9 +0,0 @@
-package com.appsdeveloperblog.app.ws.mobileappws.Cart;
-
-import com.appsdeveloperblog.app.ws.mobileappws.Product.Product;
-
-public class CartItem{
-    private String cartItemId;
-    private int cartItemQuantity;
-    private Product cartProduct;
-}

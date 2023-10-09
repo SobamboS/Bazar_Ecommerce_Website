7dcda9a38cff3b2ef84afb93b1dc83357db4bcf2
SobamboS <sobamboolusola1152@gmail.com>
2023-10-09 16:36:34 +0100

    Refactoring

--- src/main/java/com/appsdeveloperblog/app/ws/mobileappws/controller/RegistrationController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/mobileappws/controller/RegistrationController.java
@@ -2,12 +2,13 @@ package com.appsdeveloperblog.app.ws.mobileappws.controller;
 
 import com.appsdeveloperblog.app.ws.mobileappws.Utils.ApiResponse;
 import com.appsdeveloperblog.app.ws.mobileappws.dto.request.LoginRequest;
-import com.appsdeveloperblog.app.ws.mobileappws.dto.request.OtpConfirmationRequest;
+import com.appsdeveloperblog.app.ws.mobileappws.dto.request.ResendOtpRequest;
 import com.appsdeveloperblog.app.ws.mobileappws.dto.request.SignupRequest;
+import com.appsdeveloperblog.app.ws.mobileappws.service.UnauthenticatedUserService;
 import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +17,13 @@ import java.time.Instant;
 import java.time.ZonedDateTime;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("registration")
 public class RegistrationController{
 
-   private final RegistrationService registrationService;
+
+
+   private final UnauthenticatedUserService userService;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody @Valid SignupRequest signupRequest,
@@ -29,23 +33,23 @@ public class RegistrationController{
                 .isSuccessful(true)
                 .path(httpServletRequest.getRequestURI())
                 .timeStamp(Instant.now())
-                .data(registrationService.signup(signupRequest))
+                .data(userService.signup(signupRequest))
                 .build();
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
-    @PostMapping("/confirm-token")
-    public ResponseEntity<?> tokenConfirmation(@RequestBody @Valid OtpConfirmationRequest otpConfirmationRequest,
-                                               HttpServletRequest httpServletRequest) throws MessagingException{
-        ApiResponse apiResponse = ApiResponse.builder()
-                .status(HttpStatus.OK.value())
-                .isSuccessful(true)
-                .path(httpServletRequest.getRequestURI())
-                .timeStamp(Instant.now())
-                .data(registrationService.tokenConfirmation(otpConfirmationRequest))
-                .build();
-        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
-    }
+//    @PostMapping("/confirm-token")
+//    public ResponseEntity<?> tokenConfirmation(@RequestBody @Valid OtpConfirmationRequest otpConfirmationRequest,
+//                                               HttpServletRequest httpServletRequest) throws MessagingException{
+//        ApiResponse apiResponse = ApiResponse.builder()
+//                .status(HttpStatus.OK.value())
+//                .isSuccessful(true)
+//                .path(httpServletRequest.getRequestURI())
+//                .timeStamp(Instant.now())
+//                .data(userService.tokenConfirmation(otpConfirmationRequest))
+//                .build();
+//        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+//    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid LoginRequest loginRequest,
@@ -55,19 +59,19 @@ public class RegistrationController{
                 .isSuccessful(true)
                 .path(httpServletRequest.getRequestURI())
                 .timeStamp(Instant.now())
-                .data(registrationService.login(loginRequest))
+                .data(userService.login(loginRequest))
                 .build();
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
     @PostMapping("/resend-token")
-    public ResponseEntity<?> resendToken(@RequestBody @Valid ResendTokenRequest resendTokenRequest,
+    public ResponseEntity<?> resendToken(@RequestBody @Valid ResendOtpRequest resendOtpRequest,
                                          HttpServletRequest httpServletRequest) throws MessagingException{
         ApiResponse apiResponse = ApiResponse.builder()
                 .status(HttpStatus.OK.value())
                 .isSuccessful(true)
                 .path(httpServletRequest.getRequestURI())
                 .timeStamp(Instant.now())
-                .data(registrationService.resendToken(resendTokenRequest))
+                .data(userService.resendOtp(resendOtpRequest))
                 .build();
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
